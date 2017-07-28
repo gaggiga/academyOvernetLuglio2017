c391c1791eb5f3292ce82b9c8088824c036b0fc6
gaggiga <gabriele.gaggi@brain-sys.it>
2017-07-28 13:03:45 +0200

    Configurazione iniziale home page dinamica

---
+++ Overnet.SitoAcademy/Overnet.SitoAcademy/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Overnet.SitoAcademy
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            var saluto = ConfigurationManager.AppSettings["saluto"];
+            var data = DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss");
+
+            var result = $"<!DOCTYPE html><html><head><meta charset=\"utf-8\">"
+                       + $"<title>Prima prova</title></head>"
+                       + $"<body><h2>{saluto}</h2>"
+                       + $"<p>{data}</p></body></html>";
+
+            return result;
+
+            //return "Hello <strong>world</strong>";
+        }
+    }
+}
