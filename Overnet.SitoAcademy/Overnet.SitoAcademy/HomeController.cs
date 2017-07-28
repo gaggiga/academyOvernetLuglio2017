7ac89498930bac4b2629decc3760debb7143115c
gaggiga <gabriele.gaggi@brain-sys.it>
2017-07-28 14:22:49 +0200

    Separata generazione vista

--- Overnet.SitoAcademy/Overnet.SitoAcademy/HomeController.cs
+++ Overnet.SitoAcademy/Overnet.SitoAcademy/HomeController.cs
@@ -14,14 +14,17 @@ namespace Overnet.SitoAcademy
             var saluto = ConfigurationManager.AppSettings["saluto"];
             var data = DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss");
 
+            return Vista(saluto, data);
+        }
+
+        private string Vista(string saluto, string data)
+        {
             var result = $"<!DOCTYPE html><html><head><meta charset=\"utf-8\">"
                        + $"<title>Prima prova</title></head>"
                        + $"<body><h2>{saluto}</h2>"
                        + $"<p>{data}</p></body></html>";
 
             return result;
-
-            //return "Hello <strong>world</strong>";
         }
     }
 }
