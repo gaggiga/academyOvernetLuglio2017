3e8d620e6d325ef41f6e9f3daabf3d5b97672a7a
gaggiga <gabriele.gaggi@brain-sys.it>
2017-07-28 14:35:02 +0200

    Portata generazione vista in file cshtml

--- Overnet.SitoAcademy/Overnet.SitoAcademy/HomeController.cs
+++ Overnet.SitoAcademy/Overnet.SitoAcademy/HomeController.cs
@@ -9,23 +9,13 @@ namespace Overnet.SitoAcademy
 {
     public class HomeController : Controller
     {
-        public string Index()
+        public ViewResult Index()
         {
             var model = new Modello();
             model.Saluto = ConfigurationManager.AppSettings["saluto"];
             model.Data = DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss");
 
-            return Vista(model);
-        }
-
-        private string Vista(Modello model)
-        {
-            var result = $"<!DOCTYPE html><html><head><meta charset=\"utf-8\">"
-                       + $"<title>Prima prova</title></head>"
-                       + $"<body><h2>{model.Saluto}</h2>"
-                       + $"<p>{model.Data}</p></body></html>";
-
-            return result;
+            return View("~/Views/ViewPage1.cshtml", model);
         }
     }
 
