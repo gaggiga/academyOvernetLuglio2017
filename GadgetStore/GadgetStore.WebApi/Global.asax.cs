23c389af2382f8042e1d853bb73d6f0f90e19db8
gaggiga <gabriele.gaggi@brain-sys.it>
2017-08-03 15:38:09 +0200

    Ripulito progetto Api

--- GadgetStore/GadgetStore.WebApi/Global.asax.cs
+++ GadgetStore/GadgetStore.WebApi/Global.asax.cs
@@ -11,7 +11,21 @@ namespace GadgetStore.WebApi
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configure(Register);
+        }
+
+        public void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
         }
     }
 }
