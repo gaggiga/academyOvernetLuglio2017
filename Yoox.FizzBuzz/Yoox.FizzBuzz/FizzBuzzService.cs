60cb5cb42d2e49180829a2788e38bb7d72938a5e
gaggiga <gabriele.gaggi@brain-sys.it>
2017-09-04 15:12:14 +0200

    Esempio TDD FizzBuzz

---
+++ Yoox.FizzBuzz/Yoox.FizzBuzz/FizzBuzzService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yoox.FizzBuzz
+{
+    public class FizzBuzzService
+    {
+        public string Print(int v)
+        {
+            if (DivisibilePerCinque(v) && DivisibilePerTre(v))
+            {
+                return "FizzBuzz";
+            }
+            else if (DivisibilePerCinque(v))
+            {
+                return "Buzz";
+            }
+            else if (DivisibilePerTre(v))
+            {
+                return "Fizz";
+            }
+
+            return v.ToString();
+        }
+
+        private static bool DivisibilePerTre(int v)
+        {
+            return v % 3 == 0;
+        }
+
+        private static bool DivisibilePerCinque(int v)
+        {
+            return v % 5 == 0;
+        }
+    }
+}
---
+++ Yoox.FizzBuzz/Yoox.FizzBuzzTdd/FizzBuzzTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yoox.FizzBuzz;
+
+namespace Yoox.FizzBuzzTdd
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        private FizzBuzzService _fizzBuzz;
+
+        public FizzBuzzTests()
+        {
+            _fizzBuzz = new FizzBuzzService();
+        }
+
+        [TestMethod]
+        public void ShouldPrintNumber()
+        {
+            int[] numbers = new int[] { 1, 2, 4, 7, 8, 11, 13 };
+
+            foreach(var n in numbers)
+            {
+                Assert.AreEqual(n.ToString(), _fizzBuzz.Print(n));
+            }
+        }
+
+        [TestMethod]
+        public void ShouldPrintFizz()
+        {
+            int[] numbers = new int[] { 3, 6, 9, 12, 18, 21, 24, 27 };
+
+            foreach (var n in numbers)
+            {
+                Assert.AreEqual("Fizz", _fizzBuzz.Print(n));
+            }
+
+        }
+
+        [TestMethod]
+        public void ShouldPrintBuzz()
+        {
+            int[] numbers = new int[] { 5, 10, 20, 25, 35 };
+
+            foreach (var n in numbers)
+            {
+                Assert.AreEqual("Buzz", _fizzBuzz.Print(n));
+            }
+        }
+
+        [TestMethod]
+        public void ShouldPrintFizzBuzz()
+        {
+            int[] numbers = new int[] { 15, 30, 45, 60 };
+
+            foreach (var n in numbers)
+            {
+                Assert.AreEqual("FizzBuzz", _fizzBuzz.Print(n));
+            }
+        }
+    }
+}
