5718e9430ea293b03983e6bdf41ccb1a94eb3b88
Giovanni Improta <bigbossgion@gmail.com>
2017-09-05 12:52:20 +0200

    GruppOne Esercizio 3 implementato supporto a diversi delimitatori

--- Yoox.StringCalculatorKata/Yoox.StringCalculatorKata/StringCalculator_One.cs
+++ Yoox.StringCalculatorKata/Yoox.StringCalculatorKata/StringCalculator_One.cs
@@ -15,9 +15,22 @@ namespace Yoox.StringCalculatorKataOne
                 return 0;
             }
 
-            char[] delimiters = new char[] { ',', '\n' };
+            var delimiters = new List<Char>();
+            char delimiter = ',';
 
-            return numbers.Split(delimiters).Sum(s => Int32.Parse(s));
+            if(numbers.StartsWith("//"))
+            {
+                delimiter = numbers[2];
+                numbers = numbers.Substring(4);
+            }
+            else
+            {
+                delimiters.Add('\n');
+            }
+
+            delimiters.Add(delimiter);
+
+            return numbers.Split(delimiters.ToArray()).Sum(s => Int32.Parse(s));
         }
     }
 }
