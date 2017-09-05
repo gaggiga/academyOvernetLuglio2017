00d9a64f8d526657ceb43aee7b767eb310c0bb42
gaggiga <gabriele.gaggi@brain-sys.it>
2017-09-05 15:42:29 +0200

    Aggiunta extension per gestire Assert su eccezione

---
+++ Yoox.StringCalculatorKata/Yoox.StringCalculatorKataTest/AssertExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yoox.StringCalculatorKataTest
+{
+    public static class AssertExtensions
+    {
+        public static T Throws<T>(Action action) where T : Exception
+        {
+            T result = null;
+
+            try
+            {
+                action();
+                Assert.Fail("Expected exception but no exception was thrown.");
+            }
+            catch (T e)
+            {
+                Assert.IsTrue(true);
+                result = e;
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(string.Format("Expected exception of type {0} but no exception was thrown.", typeof(T)));
+            }
+
+            return result;
+        }
+    }
+}
